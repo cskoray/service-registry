611b37bdba64ecfeff817e51e10539db37d67e20
koray <k.kaya@hymnai.com>
2023-07-24 00:27:07 +0100

    service-registry initiated

---
+++ src/test/java/com/solidcode/registry/InternetBankingServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solidcode.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetBankingServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
